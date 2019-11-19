7b9fa4d6fdcddbf2015f293cfe26cc8c52cc1748
Ximon Eighteen <3304436+ximon18@users.noreply.github.com>
2019-11-19 15:59:53 +0000

    E2E test framework: update the example E2E test to use the new features and to deliberately cause the recordset.c names_recordsetexpiry() assert.

--- signer/src/test/cmocka/tests/poc_e2e_test.c
+++ signer/src/test/cmocka/tests/poc_e2e_test.c
@@ -26,22 +26,24 @@
 #include "test_framework.h"
 #include "mock_outputs.h"
 #include "signer/zone.h"
+#include <alloca.h>
+#include <stdlib.h>
 
 
-const char *test_zone = R"(
+const char *ZONE_TEMPLATE = R"(
 $TTL	86400 ; 24 hours could have been written as 24h or 1d
 ; $TTL used for all RRs without explicit TTL value
-$ORIGIN )" MOCK_ZONE_NAME R"( 
+$ORIGIN <ZONE_NAME> 
 @  1D  IN  SOA ns1.example.com. hostmaster.example.com. (
-			      2002022401 ; serial
-			      3H ; refresh
-			      15 ; retry
-			      1w ; expire
-			      3h ; nxdomain ttl
-			     )
-       IN  NS     ns1.example.com. ; in the domain
-       IN  NS     ns2.smokeyjoe.com. ; external to domain
-       IN  MX  10 mail.another.com. ; external mail provider
+                <ZONE_SERIAL>; serial
+                3H ; refresh
+                15 ; retry
+                1w ; expire
+                3h ; nxdomain ttl
+                )
+    IN  NS     ns1.example.com. ; in the domain
+    IN  NS     ns2.smokeyjoe.com. ; external to domain
+    IN  MX  10 mail.another.com. ; external mail provider
 ; server host definitions
 ns1    IN  A      192.168.0.1  ;name server definition     
 www    IN  A      192.168.0.2  ;web server definition
@@ -52,24 +54,116 @@ fred   IN  A      192.168.0.4
 )";
 
 
-DEFINE_SIGNED_ZONE_CHECK(expect_sig_count, zone->stats->sig_count, !=);
+// Define our own assert_int_equal() because with CMocka() the printed error
+// message just indicates the failing line of code, not what the line says and
+// thus nothing about what actually failed, at least not at first glance, you
+// have to go and read that line of code to understand what was being tested.
+void assert_uint32_ptr_equal(const char* field_name, uint32_t *actual, uint32_t *expected)
+{
+    if (!actual && !expected) return;
+    if ((!actual && expected) || (actual && !expected) || (*actual != *expected)) {
+        char actual_str[128], expected_str[128];
+        if (actual)   snprintf(actual_str, 128, "%d", *actual);
+        if (expected) snprintf(expected_str, 128, "%d", *expected);
+        fail_msg("%s value %s != %s", field_name,
+            actual ? actual_str : "NULL",
+            expected ? expected_str : "NULL");
+    }
+}
+
+// Define a compound checker, because with CMocka it's not possible to register
+// multiple checkers which in sequence check the same parameter (in this case
+// fields of the same struct parameter ) to the same function.
+#define ASSERT_UINT32_PTR_EQUAL(actual, expected) \
+    assert_uint32_ptr_equal(#actual, actual, expected)
+
+int check_zone_serials(zone_type *actual, zone_type *expected)
+{
+    assert_string_equal(actual->name, expected->name);
+    ASSERT_UINT32_PTR_EQUAL(actual->nextserial,     expected->nextserial);
+    ASSERT_UINT32_PTR_EQUAL(actual->inboundserial,  expected->inboundserial);
+    // ASSERT_UINT32_PTR_EQUAL(actual->outboundserial, expected->outboundserial);
+}
+
+// DEFINE_SIGNED_ZONE_CHECK_RETURNS_ZERO(expect_our_zone_is_output, strcmp(zone->name, MOCK_ZONE_NAME));
+// DEFINE_SIGNED_ZONE_CHECK_WITH_OP(expect_next_serial, zone->nextserial ? *(zone->nextserial) : fail()), !=);
+// DEFINE_SIGNED_ZONE_CHECK_WITH_OP(expect_inbound_serial, zone->inboundserial ? *(zone->inboundserial) : fail()), !=);
+// DEFINE_SIGNED_ZONE_CHECK_WITH_OP(expect_outbound_serial, zone->outboundserial ? *(zone->outboundserial) : fail()), !=);
+DEFINE_SIGNED_ZONE_CHECK_WITH_FUNC(expect_output_zone_serials, check_zone_serials);
+
+
+// This cannot be used at present with the develop branch because sig_count is
+// never updated. This is a regression compared to the 2.1.x branch.
+// DEFINE_SIGNED_ZONE_CHECK(expect_sig_count, zone->stats->sig_count, !=);
+
+
+#define DUP_SERIAL(serial) dup_serial(serial, alloca(sizeof(uint32_t)))
+uint32_t *dup_serial(uint32_t serial, uint32_t *new_mem) {
+    *new_mem = serial;
+    return new_mem;
+}
+
 
 E2E_TEST_BEGIN(load_zone_file)
-    // Given an unsigned input zone
-    e2e_configure_mocks(state, TASK_READ, test_zone);
+    const char *zone_soa_serial = "2019111801";
+    uint32_t zone_soa_serial_num = atol(zone_soa_serial);
+    const char *replacements[] = { MOCK_ZONE_NAME, zone_soa_serial };
+    const char *test_zone = from_template(ZONE_TEMPLATE, replacements);
 
-    zone_type *zone = state->zone;
+    // Given an unsigned input zone
+    zone_type *zone = e2e_configure_mocks(state, test_zone, E2E_VIEWSTATE_W_OKAY);
+
+    // When it is signed using the 'datecounter' soa serial algorithm
     zone->signconf->soa_serial = strdup("datecounter");
     zone->signconf->last_modified = 1;
     will_return_always(__wrap_time_now, __real_time_now());
 
-    assert_int_equal(0, zone->stats->sig_count);
-
-    // After signing expect: 15 RRSIGs and no errors
-    // This currently fails because sig_count is not updated in the develop
-    // branch, but is in 2.1.x.
-    // expect_sig_count(15);
+    // Expect the serial number to be
+    expect_output_zone_serials(&((zone_type) {
+        .name           = zone->name,
+        .nextserial     = NULL,
+        .inboundserial  = DUP_SERIAL(zone_soa_serial_num),
+        .outboundserial = DUP_SERIAL(zone_soa_serial_num+1)
+    }));
 
     // Go!
     e2e_go(state, TASK_READ, TASK_SIGN, TASK_WRITE, TASK_STOP);
+E2E_TEST_END
+
+
+E2E_TEST_BEGIN(load_zone_file2)
+    const char *zone_soa_serial = "2019111801";
+    uint32_t zone_soa_serial_num = atol(zone_soa_serial);
+    const char *replacements[] = { MOCK_ZONE_NAME, zone_soa_serial };
+    const char *test_zone = from_template(ZONE_TEMPLATE, replacements);
+
+    // sleeping >0.5 seconds and then loading state which contains an expiry
+    // time for a record in the zone to be signed (in this case bill.MOCKZONE.)
+    // causes an assert:
+    // src/views/recordset.c:623: names_recordsetexpiry: Assertion `record->expiry == NULL' failed.
+    usleep(600000);
+
+    // Given an unsigned input zone and prior state
+    zone_type *zone = e2e_configure_mocks(state, test_zone, E2E_VIEWSTATE_R_OKAY);
+
+    // When it is signed using the 'datecounter' soa serial algorithm
+    zone->signconf->soa_serial = strdup("datecounter");
+    zone->signconf->last_modified = 1;
+    will_return_always(__wrap_time_now, __real_time_now());
+
+    // Expect the serial number to be
+    expect_output_zone_serials(&((zone_type) {
+        .name           = zone->name,
+        .nextserial     = NULL,
+        .inboundserial  = DUP_SERIAL(zone_soa_serial_num),
+        .outboundserial = DUP_SERIAL(zone_soa_serial_num+1)
+    }));
+
+    // Go!
+    e2e_go(state, TASK_READ, TASK_SIGN, TASK_WRITE, TASK_STOP);
+E2E_TEST_END
+
+
+E2E_TEST_BEGIN(cleanup_view_state_tmp_file)
+    unlink(MOCK_VIEW_STATE_PATH);
 E2E_TEST_END
