8176fe8fb19f1047138581324b25f8fb0e36420f
Ximon Eighteen <3304436+ximon18@users.noreply.github.com>
2025-03-11 10:34:35 +0100

    More dnsi.

--- testing/test-cases.d/signer.adapters.output_basic/test.sh
+++ testing/test-cases.d/signer.adapters.output_basic/test.sh
@@ -30,11 +30,11 @@ syslog_waitfor 60 'ods-signerd: .*\[STATS\] ods' &&
 syslog_waitfor 120 'ods-signerd: .*\[notify\] notify max retry for zone ods, 127\.0\.0\.1 unreachable' &&
 
 ## SOA query
-log_this_timeout soa 10 dnsi query -p 15354 -s 127.0.0.1 ods soa &&
+log_this_timeout soa 10 dnsi query --format dig -p 15354 -s 127.0.0.1 ods soa &&
 log_grep soa stdout 'ods\..*3600.*IN.*SOA.*ns1\.ods\..*postmaster\.ods\..*1001.*9000.*4500.*1209600.*3600' &&
 
 ## See if we can transfer the signed zone
-log_this_timeout axfr 10 drill -p 15354 @127.0.0.1 axfr ods &&
+log_this_timeout axfr 10 dnsi xfr --format dig -p 15354 -s 127.0.0.1 ods &&
 log_grep axfr stdout 'ods\..*3600.*IN.*SOA.*ns1\.ods\..*postmaster\.ods\..*1001.*9000.*4500.*1209600.*3600' &&
 log_grep axfr stdout 'ods\..*600.*IN.*MX.*10.*mail\.ods\.' &&
 
@@ -42,7 +42,7 @@ log_grep axfr stdout 'ods\..*600.*IN.*MX.*10.*mail\.ods\.' &&
 log_grep axfr stdout 'below\.zonecut\.label4\.ods\..*600.*IN.*NS.*ns\.zonecut\.label4\.ods\.' &&
 
 ## See if we send overflow UDP if does not fit.
-log_this_timeout ixfr 10 drill -p 15354 @127.0.0.1 ixfr ods &&
+log_this_timeout ixfr 10 dnsi xfr --format dig -p 15354 -s 127.0.0.1 --ixfr 1000 ods &&
 syslog_waitfor 10 'ods-signerd: .*\[axfr\] axfr fallback zone ods' &&
 syslog_waitfor 10 'ods-signerd: .*\[axfr\] axfr udp overflow zone ods' &&
 log_grep ixfr stdout 'ods\..*IN.*\(TYPE251\|IXFR\)' &&
@@ -50,7 +50,7 @@ log_grep ixfr stdout 'ods\..*3600.*IN.*SOA.*ns1\.ods\..*postmaster\.ods\..*1001.
 ! (log_grep ixfr stdout 'ods\..*600.*IN.*MX.*10.*mail\.ods\.') &&
 
 ## See if we fallback to AXFR if IXFR not available.
-log_this_timeout ixfr-tcp 10 drill -t -p 15354 @127.0.0.1 ixfr ods &&
+log_this_timeout ixfr-tcp 10 dnsi xfr --format dig -t -p 15354 -s 127.0.0.1 --ixfr 1000 ods &&
 log_grep ixfr-tcp stdout 'ods\..*3600.*IN.*SOA.*ns1\.ods\..*postmaster\.ods\..*1001.*9000.*4500.*1209600.*3600' &&
 log_grep ixfr-tcp stdout 'ods\..*600.*IN.*MX.*10.*mail\.ods\.' &&
 
@@ -60,7 +60,7 @@ ods-signer sign ods &&
 syslog_waitfor 10 'ods-signerd: .*\[STATS\] ods 1002 RR\[count=3 time*' &&
 
 ## See if we can get an IXFR back
-log_this_timeout dig 10 dig -p 15354 @127.0.0.1 ixfr=1001 ods &&
+log_this_timeout dig 10 dnsi xfr --format dig -p 15354 -s 127.0.0.1 --ixfr 1001 ods &&
 log_grep dig stdout 'ods\..*3600.*IN.*SOA.*ns1\.ods\..*postmaster\.ods\..*1002.*9000.*4500.*1209600.*3600' &&
 log_grep dig stdout 'label35\.ods\..*3600.*IN.*NS.*ns1\.label35\.ods\.' &&
 log_grep dig stdout 'ns1\.label35\.ods\..*3600.*IN.*A.*192\.0\.2\.1' &&
