2fab1e4f171c39d533912b28009e6af13c8d8b75
Ximon Eighteen <3304436+ximon18@users.noreply.github.com>
2025-03-12 10:17:48 +0100

    Name forced verbosity boosting after verbosity, not log level.

--- testing/functions-opendnssec.sh
+++ testing/functions-opendnssec.sh
@@ -355,10 +355,6 @@ ods_setup_env ()
 		ods_stop_enforcer
 	fi &&
 	echo "ods_setup_env: setup env succeeded!" &&
-	if [ -n "$ODS_TEST_MIN_LOG_LEVEL" ]; then
-	    ods_enforcer_verbosity $ODS_TEST_MIN_LOG_LEVEL &&
-	    ods_signer_verbosity $ODS_TEST_MIN_LOG_LEVEL
-	fi &&
 	return 0
 
 	echo "ods_setup_env: setup failed!" >&2
@@ -1069,6 +1065,11 @@ ods_start_ods-control ()
 	ods_enforcer_waitfor_starts "$(( ODS_ENFORCER_START_COUNT + 1 ))" "$timeout" &&
 
 	echo "ods_start_ods-control: ods-control started OK" &&
+	if [ -n "$ODS_TEST_MIN_VERBOSITY" ]; then
+		echo "ods_start_ods-control: Forcing ODS verbosity to ${ODS_TEST_MIN_VERBOSITY} because env var ODS_TEST_MIN_VERBOSITY is set."
+		ods_enforcer_verbosity $ODS_TEST_MIN_VERBOSITY &&
+		ods_signer_verbosity $ODS_TEST_MIN_VERBOSITY
+	fi &&
 	return 0
 
 	echo "ods_start_ods-control: ods-control started FAILED" >&2
@@ -1750,8 +1751,11 @@ ods_set_verbosity() {
 	local daemon="$1"
 	local verbosity="$2"
 
-	if [ -n "$ODS_TEST_MIN_LOG_LEVEL" ]; then
-		verbosity=$(( verbosity > ODS_TEST_MIN_LOG_LEVEL ? verbosity : ODS_TEST_MIN_LOG_LEVEL ))
+	if [ -n "$ODS_TEST_MIN_VERBOSITY" ]; then
+		verbosity=$(( verbosity > ODS_TEST_MIN_VERBOSITY ? verbosity : ODS_TEST_MIN_VERBOSITY ))
+		if [ $verbosity -ne $2 ]; then
+			echo "ods_set_verbosity: Requested verbosity $1 increased to ${ODS_TEST_MIN_VERBOSITY} because env var ODS_TEST_MIN_VERBOSITY is set."
+		fi
 	fi
 
 	case $daemon in
